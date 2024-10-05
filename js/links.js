7567ac3219b513e512e8a4f84b7aebed2f7b85f1
brianmotta <bmotta.0726@gmail.com>
2024-10-04 21:15:55 -0500

    Emotions expanded, fewer songs per emotion

--- js/links.js
+++ js/links.js
@@ -1,17 +1,75 @@
-let links = [
-    "rXzi93yJz1I",
-    "r4l9bFqgMaQ",
-    "JwoC6HYeq_Q",
-    "yIOL5SSjWdw",
-    "1Ny1fjdLKew",
-    "0uwwMjkF8kM",
-    "_AWIqXzvX-U",
-    "dnZt474XBS0",
-    "LBt60dfwEBY",
-    "m6O9MDdXHuM",
+const euphoria_links = [
+    "X9_n8jakvWU",
+    "VFTI3uMVCOk",
+    "UxxajLWwzqY",
+    "IPKAwJKGSDc"
+]
+
+const nostalgia_links = [
+    "JHhV9TPNmTI",
+    "5BMGfLVxY9s",
+    "RBtlPT23PTM",
+    "4aeETEoNfOg",
+]
+
+const sadness_links = [
+    "Fv6sHgq6hFc",
+    "o1Xsj9-3Pvo",
+    "n5h0qHwNrHk",
+    "STugQ0X1NoI"
+];
+
+const joy_links = [
+    "eHT5KNoWjzY",
+    "vOhLbA-B-bE",
+    "iRVzRhKF5Fk",
+    "tiDmy6XnNyA"
+
+];
+
+const dreaminess_links = [
     "rPMvuHRao00",
-    "my1B2ldiiW8",
-    "etmFT1OQSnc",
-    "Y8wifV5RYr8",
-    "_1_IXFQY5Wk"
+    "W9NVtwtSEzQ",
+    "C6ZvOCYSOVQ",
+    "WMU91F97xBU"
+];
+
+const calm_links = [
+    "TZip_br_v3w",
+    "WuenyQ4NCQE",
+    "LTrk4X9ACtw",
+    "aIHF7u9Wwiw"
+];
+
+const anxiety_links = [
+    "PGtt7quBNjw",
+    "2atkj_KWLl0",
+    "rXzi93yJz1I",
+    "liG_EuqHr4U"
+];
+
+const triumph_links = [
+    "ntG50eXbBtc",
+    "e8xni3EcIbc",
+    "Y66j_BUCBMY",
+    "dvgZkm1xWPE"
+];
+
+const mindfulness_links = [
+    "GfITojs_mNg",
+    "pT68FS3YbQ4",
+    "PZqQT5904_U",
+    "1Ny1fjdLKew"
+];
+
+const links_list = [
+    euphoria_links,
+    nostalgia_links,
+    sadness_links,
+    joy_links,
+    dreaminess_links,
+    calm_links,
+    anxiety_links,
+    triumph_links,
+    mindfulness_links
 ]
